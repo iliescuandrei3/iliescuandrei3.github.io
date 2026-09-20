5c55d029110ad2a0b614dcb23d0987302ca903c6
Andrei Iliescu <andrei.iliescu03@gmail.com>
2026-09-20 23:10:12 +0300

    add deploy files

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://iliescuandrei3.github.io',
   integrations: [react(), mdx()],
 
   vite: {
